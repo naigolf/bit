9780141c7897365e67628ed9ffa5838f4b8b0b03
nai golf <niy_golf@hotmail.com>
2022-05-01 00:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var API_SECRET = "b"+SECRETkey
 var ts;	
 var signature;
 
-
+/*
 function separators(data){
 var uu = {"amt":10,
 	  "rat":0,
@@ -47,17 +47,40 @@ var uu = {"amt":10,
 	 }
 return 	uu
 }
+*/
 
-function sign(data){
-var j = separators(data);
+
+function sign(dataget){
 	
-console.log('Signing payload: ' + JSON.stringify(j))
+	///////////////////////////////////////////////
+	
+var timestampEncode = convert.utf8.encode(convert.jsonEncode(ts));
+var secretEncode = convert.utf8.encode(API_SECRET);
 
-var hmac = crypto.createHmac('sha256', API_SECRET )
-                 .update(JSON.stringify(j))
-                 .digest('hex')
-console.log('hmac :' + hmac)
-return hmac
+console.log('timestampEncode: ' + timestampEncode)
+console.log('secretEncode: ' + secretEncode)	
+	
+var hmacSha256 = new Hmac(sha256, secretEncode); // HMAC-SHA256
+var digest = hmacSha256.convert(timestampEncode);
+	
+console.log('digest: ' + digest.toString())	
+	
+return digest.toString();
+	
+	
+	/////////////////////////////////////////////////
+	
+	
+	
+//var j = separators(data);
+	
+///////console.log('Signing payload: ' + JSON.stringify(j))
+
+//var hmac = crypto.createHmac('sha256', API_SECRET )
+                 //.update(JSON.stringify(j))
+                 //.digest('hex')
+//console.log('hmac :' + hmac)
+//return hmac
 }
 
 
@@ -97,9 +120,12 @@ let data = {
 
 //console.log('data === ' + JSON.stringify(data))
 
-signature = sign(data);
+signature = sign(dataget);
 //data = {'sig' : signature}
-
+payload['sig'] = signature;
+String payLoadJson = convert.jsonEncode(payload);
+	    
+	    
 
 
 //['sig'] = signature
@@ -113,12 +139,13 @@ console.log('Payload with signature: ' + JSON.stringify(data))
 	
 	    
 request.post({
-        url: API_HOST + '/api/market/place-bid/test',
+        url: API_HOST + '/api/market/place-bid',
         headers: header,
         data: JSON.stringify(data)
     }, function (error, response, body){
+	console.log('Response: ' + body)
     if (!error && response.statusCode == 200) {                      
-      console.log('Response: ' + body)
+      console.log('Response:  == 200 ' + body)
 res.end(body)	    
     }
 }
